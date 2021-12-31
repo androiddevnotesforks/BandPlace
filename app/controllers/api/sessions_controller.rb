c5543311bd939fbb4fc197895070d85ea52513b0
sam <s.gelernter@gmail.com>
2021-12-31 10:51:08 -0500

    Working on no user logout error

--- app/controllers/api/sessions_controller.rb
+++ app/controllers/api/sessions_controller.rb
@@ -3,7 +3,12 @@ class Api::SessionsController < ApplicationController
     end
 
     def destroy
-
-        render json: {}
+        if logged_in?
+            logout!
+            render json: {}
+        else
+            flash.alert = 'Nobody is logged in!'   
+            render json: 'Nobody is logged in!', status: 404     
+        end
     end
 end
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,16 +2,24 @@ class ApplicationController < ActionController::Base
 
     helper_method :current_user, :logged_in?
     
+    # REMOVE AFTER TESTING VERY IMPORTANTE
+    skip_before_action :verify_authenticity_token
+    
     def current_user 
+        @current_user ||= User.find_by(session_token: session[:session_token])
     end
 
     def logged_in?
+        !!current_user
     end
 
     def login!(user)
+        session[:session_token] = user.reset_session_token!
     end
 
     def logout!
+        current_user.reset_session_token!
+        session[:session_token] = nil
     end
 
 end
