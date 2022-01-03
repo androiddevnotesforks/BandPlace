806a3c60e6f3c6f3b1b6172c7fd0fcd14a19bbf0
sam <s.gelernter@gmail.com>
2022-01-02 21:00:41 -0500

    Add auth/protected routes

--- frontend/App.jsx
+++ frontend/App.jsx
@@ -3,7 +3,8 @@ import NavBar from "./components/nav_bar";
 import LoginFormContainer from "./components/auth/login_form_container";
 import SignupFormContainer from "./components/auth/artist_signup_form_container";
 import Splash from "./components/splash";
-import { Route, Switch } from "react-router-dom";
+import { Switch, Route } from "react-router-dom";
+import { AuthRoute, ProtectedRoute } from "./util/route_util";
 
 const App = () => (
     <div>
@@ -12,8 +13,8 @@ const App = () => (
             <NavBar />
         </header>
         <Switch>
-            <Route path='/login' component={LoginFormContainer} />
-            <Route path='/signup' component={SignupFormContainer} />
+            <AuthRoute path='/login' component={LoginFormContainer} />
+            <AuthRoute path='/signup' component={SignupFormContainer} />
             <Route path='/' component={Splash} />
         </Switch>
     </div>
