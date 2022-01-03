44c2bfc477a4f37ddc0229d415aaa806597a7bb3
sam <s.gelernter@gmail.com>
2022-01-02 20:48:56 -0500

    Bootstrap current user

--- app/views/api/users/show.json.jbuilder
+++ app/views/api/users/show.json.jbuilder
@@ -1,3 +1,3 @@
 # TO-DO: GET RELEASE IDS FROM ASSOCIATION WHEN THAT'S SET UP
 
-json.extract! @user, :id,  :email, :username, :is_artist, :profile_image_url, :bio
+json.partial! 'user', user: @user
--- frontend/bandplace.jsx
+++ frontend/bandplace.jsx
@@ -8,11 +8,25 @@ import { login, logout, signup } from "./actions/session_actions";
 
 document.addEventListener("DOMContentLoaded", () => {
 
-    [window.login, window.logout, window.signup] = [login, logout, signup];
+        [window.login, window.logout, window.signup] = [login, logout, signup];
     
     const rootEl = document.getElementById('root');
-    const store = configureStore();
-    window.getState = store.getState;
-    window.dispatch = store.dispatch;
+
+    let store;
+    if (window.currentUser) {
+        const preloadedState = {
+            entities: {
+                users: { [window.currentUser.id]: window.currentUser }
+            },
+            session: { id: window.currentUser.id }
+        };
+        store = configureStore(preloadedState);
+    } else {
+        store = configureStore();
+    }
+
+        window.getState = store.getState;
+        window.dispatch = store.dispatch;
+    
     ReactDOM.render(<Root store={store} />, rootEl);
 })
