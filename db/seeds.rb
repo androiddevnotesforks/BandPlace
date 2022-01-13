12212d6bece57f1e379f6bc824e0be2dfbcaa1ab
sam <s.gelernter@gmail.com>
2022-01-13 18:02:35 -0500

    Add complete seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,18 +4,24 @@ require 'open-uri'
 User.destroy_all
 
 # USERS
-u1 = User.create!(email: 'test@tests.test', username: 'test', is_artist: true, bio: 'This is a test artist', password: '123123', color_profile: '#ffffff/#363636/#888888/#0687f5/#f8f8f8/#f2f2f2')
-u2 = User.create!(email: 'abc@123.biz', username: 'rotary_fan', is_artist: false, bio: 'This is a test fan', password: '123123', color_profile: '#ffffff/#363636/#888888/#0687f5/#f8f8f8/#f2f2f2')
+u1 = User.create!(email: 'elsilvercabs@cabs.cabs', username: 'El Silver Cabs', is_artist: true, bio: 'Laura - Vocals, Jake - Vocals/Bass, Zack - Guitar/Vocals, Mike - Guitar, Michael - Drums', password: 'schway', color_profile: '#ffffff/#363636/#888888/#0687f5/#f8f8f8/#f2f2f2')
+u2 = User.create!(email: 'leaked@leakthis.biz', username: 'Leaked', is_artist: false, bio: 'Not available in shops', password: 'sweatshirt', color_profile: '#ffffff/#363636/#888888/#0687f5/#f8f8f8/#f2f2f2')
 u3 = User.create!(email: 'cosmicdancer@jeepster.gov', username: 'T. Rex', is_artist: true, bio: "Life's a gas", password: 'rawramp', color_profile: '#ffffff/#363636/#888888/#0687f5/#f8f8f8/#f2f2f2')
+u4 = User.create!(email: 'yourrizzos@rizbiz.org', username: 'The Rizzos', is_artist: false, bio: "We're a gang", password: 'howitwas', color_profile: '#ffffff/#363636/#888888/#0687f5/#f8f8f8/#f2f2f2')
 
 Release.destroy_all 
 
 r1 = Release.create!(title: 'Electric Warrior', description: 'One of the greatest glam-rock records ever recorded - 1971', artist_id: u3.id)
 r2 = Release.create!(title: '20th Century Boy (Single)', artist_id: u3.id)
 r3 = Release.create!(title: 'Solid Gold Easy Action (Single)', artist_id: u3.id)
-r4 = Release.create!(title: 'Dandy In The Underworld', artist_id: u3.id)
+r5 = Release.create!(title: 'El Silver Cabs', artist_id: u1.id)
+r6 = Release.create!(title: 'Leaked', artist_id: u2.id)
+r7 = Release.create!(title: 'How It Was', artist_id: u4.id)
+
 
 Song.destroy_all 
+
+# T REX
 
 s1 = Song.create!(name: 'Mambo Sun', track: 1, release_id: r1.id)
 s2 = Song.create!(name: 'Cosmic Dancer', track: 2, release_id: r1.id)
@@ -86,7 +92,38 @@ Hey babe i'm wanna be your man
 It's plain to see you, were meant for me,
 Yeah I'm your toy Your twentieth century boy.
 20th century toy I wanna be your boy", release_id: r2.id)
-s19 = Song.create!(name: 'Teen Riot Structure', track: 1, release_id: r4.id)
+
+# CABS
+
+s20 = Song.create!(name: 'Figurines', track: 1, release_id: r5.id)
+s21 = Song.create!(name: 'Shrink', track: 2, release_id: r5.id)
+s22 = Song.create!(name: 'Bloom', track: 3, release_id: r5.id)
+s23 = Song.create!(name: 'Treading Water', track: 4, release_id: r5.id)
+s24 = Song.create!(name: "Wouldn't You Rather", track: 5, release_id: r5.id)
+s25 = Song.create!(name: 'Gun', track: 6, release_id: r5.id)
+s26 = Song.create!(name: 'Pretty Lady', track: 7, release_id: r5.id)
+s27 = Song.create!(name: 'Weekend', track: 8, release_id: r5.id)
+
+# LEAKED 
+
+s28 = Song.create!(name: 'Hanging Around', track: 1, release_id: r6.id)
+s29 = Song.create!(name: 'Fever', track: 2, release_id: r6.id)
+s30 = Song.create!(name: 'Teacup', track: 3, release_id: r6.id)
+s31 = Song.create!(name: 'Sweatshirt', track: 4, release_id: r6.id)
+
+
+# RIZZOS
+
+s32 = Song.create!(name: 'Lost Boys', track: 1, release_id: r7.id)
+s33 = Song.create!(name: 'Way Out', track: 2, release_id: r7.id)
+s34 = Song.create!(name: 'Breslin', track: 3, release_id: r7.id)
+s35 = Song.create!(name: 'Worth It', track: 4, release_id: r7.id)
+s36 = Song.create!(name: 'Hamjrgers', track: 5, release_id: r7.id)
+s37 = Song.create!(name: 'Nowhere In Particular', track: 6, release_id: r7.id)
+s38 = Song.create!(name: 'Heavy Song', track: 7, release_id: r7.id)
+s39 = Song.create!(name: 'Crybaby', track: 8, release_id: r7.id)
+s40 = Song.create!(name: 'Joey For President', track: 9, release_id: r7.id)
+s41 = Song.create!(name: 'Long Dog', track: 10, release_id: r7.id)
 
 
 # ADD SEED IMAGES
@@ -96,17 +133,32 @@ banner_img = URI.open('https://bandplace-seeds.s3.amazonaws.com/images/trex_bann
 e_warrior = URI.open('https://bandplace-seeds.s3.amazonaws.com/images/electric_warrior.jpg')
 century_boy = URI.open('https://bandplace-seeds.s3.amazonaws.com/images/20th_century.jpg')
 solid_gold = URI.open('https://bandplace-seeds.s3.amazonaws.com/images/solid_gold_square.jpg')
-dandy = URI.open('https://bandplace-seeds.s3.amazonaws.com/images/dandy.jpg')
+cabs_prof = URI.open('https://bandplace-seeds.s3.amazonaws.com/images/cabs_profile.jpg')
+cabs_cover = URI.open('https://bandplace-seeds.s3.amazonaws.com/images/el_silver_cabs_cover.jpeg')
+leaked_prof = URI.open('https://bandplace-seeds.s3.amazonaws.com/images/leaked_prof.jpg')
+leaked_cover = URI.open('https://bandplace-seeds.s3.amazonaws.com/images/leaked_square.jpg')
+rizzos_banner = URI.open('https://bandplace-seeds.s3.amazonaws.com/images/rizzos_banner.png')
+rizzos_prof = URI.open('https://bandplace-seeds.s3.amazonaws.com/images/rizzos_prof.jpg')
+rizzos_cover = URI.open('https://bandplace-seeds.s3.amazonaws.com/images/how_it_was_square.jpg')
 
 u3.profile_image.attach(io: prof_img, filename: 'trex_profile.jpg')
 u3.banner_image.attach(io: banner_img, filename: 'trex_banner.jpg')
+u1.profile_image.attach(io: cabs_prof, filename: 'cabs_profile.jpg')
+u2.profile_image.attach(io: leaked_prof, filename: 'leaked_prof.jpg')
+u4.profile_image.attach(io: rizzos_prof, filename: 'rizzos_prof.jpg')
+u4.banner_image.attach(io: rizzos_banner, filename: 'rizzos_banner.jpg')
 
 r1.cover_image.attach(io: e_warrior, filename: 'electric_warrior.jpg')
 r2.cover_image.attach(io: century_boy, filename: '20th_century.jpg')
 r3.cover_image.attach(io: solid_gold, filename: 'solid_gold.jpg')
-r4.cover_image.attach(io: dandy, filename: 'dandy.jpg')
+r5.cover_image.attach(io: cabs_cover, filename: 'el_silver_cabs_cover.jpg')
+r6.cover_image.attach(io: leaked_cover, filename: 'leaked_square.jpg')
+r7.cover_image.attach(io: rizzos_cover, filename: 'how_it_was_square.jpg')
+
 
 # ADD SEED AUDIO 
+
+# T REX
 
 mambo = URI.open('https://bandplace-seeds.s3.amazonaws.com/audio/mambo_sun.mp3')
 cosmic = URI.open('https://bandplace-seeds.s3.amazonaws.com/audio/cosmic_dancer.mp3')
@@ -125,6 +177,7 @@ acoustic = URI.open('https://bandplace-seeds.s3.amazonaws.com/audio/planet_queen
 hot = URI.open('https://bandplace-seeds.s3.amazonaws.com/audio/hot_love.mp3')
 woodland = URI.open('https://bandplace-seeds.s3.amazonaws.com/audio/woodland_rock.mp3')
 easy = URI.open('https://bandplace-seeds.s3.amazonaws.com/audio/solid_gold_easy_action.mp3')
+century = URI.open('https://bandplace-seeds.s3.amazonaws.com/audio/century_boy.mp3')
 
 s1.track_audio.attach(io: mambo, filename: 'mambo_sun.mp3')
 s2.track_audio.attach(io: cosmic, filename: 'cosmic_dancer.mp3')
@@ -143,3 +196,63 @@ s14.track_audio.attach(io: acoustic, filename: 'planet_queen_acoustic.mp3')
 s15.track_audio.attach(io: hot, filename: 'hot_love.mp3')
 s16.track_audio.attach(io: woodland, filename: 'woodland_rock.mp3')
 s17.track_audio.attach(io: easy, filename: 'solid_gold_easy_action.mp3')
+s18.track_audio.attach(io: century, filename: 'century_boy.mp3')
+
+# CABS
+
+figurines = URI.open('https://bandplace-seeds.s3.amazonaws.com/audio/01_Figurines_05_02_17_MSTR.wav')
+shrink = URI.open('https://bandplace-seeds.s3.amazonaws.com/audio/02_Shrink_05_02_17_MSTR.wav')
+bloom = URI.open('https://bandplace-seeds.s3.amazonaws.com/audio/03_Bloom_05_02_17_MSTR.wav')
+treading = URI.open('https://bandplace-seeds.s3.amazonaws.com/audio/treading_water.wav')
+rather = URI.open('https://bandplace-seeds.s3.amazonaws.com/audio/wouldnt_you_rather.wav')
+gun = URI.open('https://bandplace-seeds.s3.amazonaws.com/audio/06_Gun_05_02_17_MSTR.wav')
+pretty = URI.open('https://bandplace-seeds.s3.amazonaws.com/audio/pretty_lady.wav')
+weekend = URI.open('https://bandplace-seeds.s3.amazonaws.com/audio/08_Weekend_05_02_17_MSTR.wav')
+
+
+s20.track_audio.attach(io: figurines, filename: '01_Figurines_05_02_17_MSTR.wav')
+s21.track_audio.attach(io: shrink, filename: '02_Shrink_05_02_17_MSTR.wav')
+s22.track_audio.attach(io: bloom, filename: '03_Bloom_05_02_17_MSTR.wav')
+s23.track_audio.attach(io: treading, filename: 'treading_water.wav')
+s24.track_audio.attach(io: rather, filename: 'wouldnt_you_rather.wav')
+s25.track_audio.attach(io: gun, filename: '06_Gun_05_02_17_MSTR.wav')
+s26.track_audio.attach(io: pretty, filename: 'pretty_lady.wav')
+s27.track_audio.attach(io: weekend, filename: '08_Weekend_05_02_17_MSTR.wav')
+
+
+# LEAKED
+
+hanging = URI.open('https://bandplace-seeds.s3.amazonaws.com/audio/hanging_around.mp3')
+fever = URI.open('https://bandplace-seeds.s3.amazonaws.com/audio/Gelernter_Fever_MasteringFinalProject_OMPRD-662.mp3')
+teacup = URI.open('https://bandplace-seeds.s3.amazonaws.com/audio/Gelernter_Teacup_MasteringFinalProject_OMPRD-662.mp3')
+sweatshirt = URI.open('https://bandplace-seeds.s3.amazonaws.com/audio/Gelernter_Sweatshirt_MasteringFinalProject_OMPRD-662.mp3')
+
+s28.track_audio.attach(io: hanging, filename: 'hanging_around.mp3')
+s29.track_audio.attach(io: fever, filename: 'Gelernter_Fever_MasteringFinalProject_OMPRD-662.mp3')
+s30.track_audio.attach(io: teacup, filename: 'Gelernter_Teacup_MasteringFinalProject_OMPRD-662.mp3')
+s31.track_audio.attach(io: sweatshirt, filename: 'Gelernter_Sweatshirt_MasteringFinalProject_OMPRD-662.mp3')
+
+
+# RIZZOS
+
+lost = URI.open('https://bandplace-seeds.s3.amazonaws.com/audio/lostboys.wav')
+way = URI.open('https://bandplace-seeds.s3.amazonaws.com/audio/wayout.wav')
+breslin = URI.open('https://bandplace-seeds.s3.amazonaws.com/audio/03_breslin_finalmasterdigitaldistro.wav')
+worth = URI.open('https://bandplace-seeds.s3.amazonaws.com/audio/worthit.wav')
+hamjrgers = URI.open('https://bandplace-seeds.s3.amazonaws.com/audio/05_hamjrgers_finalmasterdigitaldistro.wav')
+nowhere = URI.open('https://bandplace-seeds.s3.amazonaws.com/audio/nowhere_particular.wav')
+heavysong = URI.open('https://bandplace-seeds.s3.amazonaws.com/audio/heavysong.wav')
+crybaby = URI.open('https://bandplace-seeds.s3.amazonaws.com/audio/08_crybaby_finalmasterdigitaldistro.wav')
+joey = URI.open('https://bandplace-seeds.s3.amazonaws.com/audio/joeyforprez.wav')
+long = URI.open('https://bandplace-seeds.s3.amazonaws.com/audio/longdog.wav')
+
+s32.track_audio.attach(io: lost, filename: 'lostboys.wav')
+s33.track_audio.attach(io: way, filename: 'wayout.wav')
+s34.track_audio.attach(io:breslin , filename: '03_breslin_finalmasterdigitaldistro.wav')
+s35.track_audio.attach(io: worth, filename: 'worthit.wav')
+s36.track_audio.attach(io: hamjrgers, filename: '05_hamjrgers_finalmasterdigitaldistro.wav')
+s37.track_audio.attach(io: nowhere, filename: 'nowhere_particular.wav')
+s38.track_audio.attach(io: heavysong, filename: 'heavysong.wav')
+s39.track_audio.attach(io: crybaby, filename: '08_crybaby_finalmasterdigitaldistro.wav')
+s40.track_audio.attach(io: joey, filename: 'joeyforprez.wav')
+s41.track_audio.attach(io: long, filename: 'longdog.wav')
--- frontend/components/splash/top_stories.jsx
+++ frontend/components/splash/top_stories.jsx
@@ -4,20 +4,28 @@ const TopStories = () => (
     <div className="top-stories-bar">
         <div className="top-stories">
             <div className="featured-story">
-                <h3>Featured Story Headline</h3>
-                <span>Featured story text.</span>
+                <div className="story-text">
+                    <h3>Remember Grunge?</h3>
+                    <span>No seriously, do you? No one will tell me what grunge is. Is it a feeling? A band? Please email me if you know!!!</span>
+                </div>
             </div>
             <div className="story">
-                <h4>Story headline</h4>
-                <span> STORY DETAILS </span>
+                <div className="story-text">
+                    <h4>Alternate History:</h4>
+                    <span> WHAT WOULD IT SOUND LIKE IF THE ROLLING STONES WERE STILL ALIVE? </span>    
+                </div>
             </div>
             <div className="story">
-                <h4>Story headline</h4>
-                <span> STORY DETAILS </span>
+                <div className="story-text">      
+                    <h4>T. Rex: The Demo User That Users Love</h4>
+                    <span> HIT THAT LOGIN BUTTON TO CHECK OUT THE HYPE </span>
+                </div>
             </div>
             <div className="story">
-                <h4>Story headline</h4>
-                <span> STORY DETAILS </span>
+                <div className="story-text">
+                    <h4>Bandplace Semi-Anually</h4>
+                    <span> NEW TUNES? MAYBE! TOO BAD THESE AREN'T CLICKABLE :( </span>
+                </div>
             </div>
         </div>
     </div>
